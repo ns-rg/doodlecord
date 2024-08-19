9b37e1386a1b06f2902da272a4095105e5b1cf9a
NisargPatel14 <nisjp.14@gmail.com>
2024-08-19 11:39:35 +0530

    Prepared the folder structure for authentication using Clerk.

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+const Authlayout = ({children}:{children:React.ReactNode}) => {
+  return (
+    <div className="bg-red-300 h-full">
+      {children}
+    </div>
+  )
+}
+
+export default Authlayout
